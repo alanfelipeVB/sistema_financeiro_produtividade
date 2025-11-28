824b8cfafacf20afc5e36a3bd3cf556b8f81d70d
alanfelipeVB <alanfelipe2000m@gmail.com>
2025-11-28 10:33:55 -0300

    update para teste online

--- app/Views/common/menu.php
+++ app/Views/common/menu.php
@@ -5,10 +5,6 @@
         <li><a href="<?= base_url('transactions') ?>">💸 Transações</a></li>
         <li><a href="<?= base_url('accounts') ?>">🏦 Contas</a></li>
         <li><a href="<?= base_url('categories') ?>">🏷️ Categorias</a></li>
-        <li><a href="<?= base_url('budgets') ?>">📊 Orçamentos</a></li>
-        <li><a href="<?= base_url('tasks') ?>">📝 Tarefas</a></li>
-        <li><a href="<?= base_url('events') ?>">📅 Eventos</a></li>
-        <li><a href="<?= base_url('notes') ?>">📚 Notas</a></li>
         <li><a href="<?= base_url('logout') ?>" class="logout">🚪 Sair</a></li>
     </ul>
 </div>
